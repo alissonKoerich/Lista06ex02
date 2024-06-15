10c408b01ef4ffca4270e049bf49e60f74baf621
Alisson Koerich <alissonkoerich5@gmail.com>
2024-06-14 23:16:58 -0300

    refresh Tests

--- src/test/java/EmployeeTest.java
+++ src/test/java/EmployeeTest.java
@@ -23,31 +23,4 @@ public class EmployeeTest {
         assertEquals(3914.36, employee.calculateIncome());
     }
 
-
-
-
-    @Test
-    void shareholderIncomeCalculationWith20participation(){
-
-        var shareholder = new Shareholder("Alisson","11111111","11111111",LocalDate.of(2004, 8, 12),
-                Sex.MALE,20.0,800.0);
-
-        assertEquals(160.0,shareholder.calculateIncome());
-
-
-    }
-
-    @Test
-    void shareholderIncomeCalculationWith70participation(){
-
-        var shareholder = new Shareholder("Alisson","11111111","11111111",LocalDate.of(2004, 8, 12),
-                Sex.MALE,70.0,2000.0);
-
-        assertEquals(14000.0,shareholder.calculateIncome());
-
-
-    }
-
-
-
 }
---
+++ src/test/java/ShareholderTest.java
@@ -0,0 +1,33 @@
+import br.cesul.ex02.Sex;
+import br.cesul.ex02.Shareholder;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ShareholderTest {
+
+
+    @Test
+    void shareholderIncomeCalculationWith20participation(){
+
+        var shareholder = new Shareholder("Alisson","11111111","11111111", LocalDate.of(2004, 8, 12),
+                Sex.MALE,20.0,800.0);
+
+        assertEquals(160.0,shareholder.calculateIncome());
+
+
+    }
+
+    @Test
+    void shareholderIncomeCalculationWith70participation(){
+
+        var shareholder = new Shareholder("Alisson","11111111","11111111",LocalDate.of(2004, 8, 12),
+                Sex.MALE,70.0,2000.0);
+
+        assertEquals(14000.0,shareholder.calculateIncome());
+
+
+    }
+}
